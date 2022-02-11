7d74970ad56fb8f710e1e3a016e51c755114a9f1
Sebastian Dehn <sdehn@redhat.com>
2022-02-11 15:10:30 +0100

    Added log4shell vulnerability for workshop

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,9 +4,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 @Path("/hello")
 public class GreetingResource {
+    private Logger logger = LogManager.getLogger(GreetingResource.class.getName());
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
